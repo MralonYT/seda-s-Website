83fa4c0dd383e93896edc5dcbaec54382ccf237a
MralonYT <Marlon.Appel@outlook.de>
2024-11-22 17:05:59 +0100

    enabling submit button code

--- index.js
+++ index.js
@@ -7,10 +7,9 @@
 //document.getElementById("myH1").textContent = "Hiii my baby";
 //document.getElementById("myP").textContent = "Ich mag Seda";
 
-//let username;
+let username;
 
-//document.getElementById("mySubmit").onclick = function(){
-//    username = document.getElementById("myText").value;
-//    document.getElementById("myh1").textContent = `Hallo ${username}`;
-//    window.location.href = `https://sedameinegrosseliebe.com/ilysm`;
-//}
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myh1").textContent = `Hallo ${username}`;
+}
