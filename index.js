9487a85880357c75efa56b373b753be690f5157f
MralonYT <Marlon.Appel@outlook.de>
2024-11-23 18:56:04 +0100

    feat: add banner with close functionality and improve layout styles

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-//console.log(`Hello`);
-//console.log(`I like Pizza`);
-
-//window.alert(`This is an alert!`);
-//window.alert(`Seda ist meine große Liebe <3`);
-
-//document.getElementById("myH1").textContent = "Hiii my baby";
-//document.getElementById("myP").textContent = "Ich mag Seda";
-
 let username;
 
 document.getElementById("mySubmit").onclick = function(){
     username = document.getElementById("myText").value;
     document.getElementById("myh1").textContent = `Hallo ${username}`;
 }
+
+document.querySelector(".BannerClose").addEventListener("click", function(){
+    this.closest(".BannerTop").style.display = "none";
+});
