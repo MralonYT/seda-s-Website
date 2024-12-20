9553b9163c59ef8e3f1314b5b78b797f9b5ba370
MralonYT <Marlon.Appel@outlook.de>
2024-12-20 07:29:53 +0100

    fix: update heading text in clue3 and correct redirect URL case in index.js

--- clue3/index.js
+++ clue3/index.js
@@ -3,7 +3,7 @@ let password;
 document.getElementById("mySubmit").onclick = function(){
     password = document.getElementById("myText").value;
     if(password == 642023322024){
-        window.location.replace("https://www.sedameinegrosseliebe.com/finish");
+        window.location.replace("https://www.sedameinegrosseliebe.com/Finish");
     }
 }
 
